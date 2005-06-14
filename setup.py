822718418d25d3d1739efefeb37414cce7670127
mpm <mpm@selenic.com>
2005-06-14 13:28:42 -0800

    Add a simple testing framework
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA1
    
    Add a simple testing framework
    
    manifest hash: 9eeea72f2f33438040998a190183958764232ece
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v1.4.0 (GNU/Linux)
    
    iD8DBQFCr0wKywK+sNU5EO8RAl9FAJ4o1QUA/YE2hCSlUPngR8h30hT1xQCgoEhu
    um2QkJOc2Rz7i6xTGPxuqzU=
    =YyUM
    -----END PGP SIGNATURE-----

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# This is the mercurial setup script. 
+#
+# './setup.py install', or
+# './setup.py --help' for more options
+
+import glob
+from distutils.core import setup, Extension
+from distutils.command.install_data import install_data
+
+class install_package_data(install_data):
+    def finalize_options(self):
+        self.set_undefined_options('install',
+                                   ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+setup(name='mercurial',
+      version='0.5b',
+      author='Matt Mackall',
+      author_email='mpm@selenic.com',
+      url='http://selenic.com/mercurial',
+      description='scalable distributed SCM',
+      license='GNU GPL',
+      packages=['mercurial'],
+      ext_modules=[Extension('mercurial.mpatch', ['mercurial/mpatch.c'])],
+      data_files=[('mercurial/templates',
+                   ['templates/map'] +
+                   glob.glob('templates/map-*') +
+                   glob.glob('templates/*.tmpl'))], 
+      cmdclass = { 'install_data' : install_package_data },
+      scripts=['hg', 'hgmerge'])
