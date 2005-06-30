84579dbab041732c89900228609344be88007c17
Thomas Arendsen Hein <thomas@intevation.de>
2005-06-30 07:16:05 +0100

    Added support for hg:// and old-http:// to 'hg clone'.
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA1
    
    Added support for hg:// and old-http:// to 'hg clone'.
    
    manifest hash: 4292cc7927afbec5663b5fff4317a238fb65cdec
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v1.4.1 (GNU/Linux)
    
    iD8DBQFCw44lW7P1GVgWeRoRArWzAJ9AjiCF7poldqFxshV+lAyHZKy2PACgpCt1
    KTdmdp+AL6m953NnYc5QNbo=
    =BWbr
    -----END PGP SIGNATURE-----

---
+++ mercurial/util.py
@@ -0,0 +1,89 @@
+# util.py - utility functions and platform specfic implementations
+#
+# Copyright 2005 K. Thananchayan <thananck@yahoo.com>
+#
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+
+import os
+
+class CommandError(Exception): pass
+
+def explain_exit(code):
+    """return a 2-tuple (desc, code) describing a process's status"""
+    if os.WIFEXITED(code):
+        val = os.WEXITSTATUS(code)
+        return "exited with status %d" % val, val
+    elif os.WIFSIGNALED(code):
+        val = os.WTERMSIG(code)
+        return "killed by signal %d" % val, val
+    elif os.WIFSTOPPED(code):
+        val = os.STOPSIG(code)
+        return "stopped by signal %d" % val, val
+    raise ValueError("invalid exit code")
+
+def system(cmd, errprefix=None):
+    """execute a shell command that must succeed"""
+    rc = os.system(cmd)
+    if rc:
+        errmsg = "%s %s" % (os.path.basename(cmd.split(None, 1)[0]),
+                            explain_exit(rc)[0])
+        if errprefix:
+            errmsg = "%s: %s" % (errprefix, errmsg)
+        raise CommandError(errmsg)
+
+def rename(src, dst):
+    try:
+        os.rename(src, dst)
+    except:
+        os.unlink(dst)
+        os.rename(src, dst)
+
+# Platfor specific varients
+if os.name == 'nt':
+    nulldev = 'NUL:'
+
+    def is_exec(f, last):
+        return last
+
+    def set_exec(f, mode):
+        pass
+
+    def pconvert(path):
+        return path.replace("\\", "/")
+
+    def makelock(info, pathname):
+        ld = os.open(pathname, os.O_CREAT | os.O_WRONLY | os.O_EXCL)
+        os.write(ld, info)
+        os.close(ld)
+
+    def readlock(pathname):
+        return file(pathname).read()
+
+else:
+    nulldev = '/dev/null'
+
+    def is_exec(f, last):
+        return (os.stat(f).st_mode & 0100 != 0)
+
+    def set_exec(f, mode):
+        s = os.stat(f).st_mode
+        if (s & 0100 != 0) == mode:
+            return
+        if mode:
+            # Turn on +x for every +r bit when making a file executable
+            # and obey umask.
+            umask = os.umask(0)
+            os.umask(umask)
+            os.chmod(f, s | (s & 0444) >> 2 & ~umask)
+        else:
+            os.chmod(f, s & 0666)
+
+    def pconvert(path):
+        return path
+
+    def makelock(info, pathname):
+        os.symlink(info, pathname)
+
+    def readlock(pathname):
+        return os.readlink(pathname)
