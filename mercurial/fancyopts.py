c1175cf1ece39f5921aa4f31bcbdc8ad5566e806
TK Soh <teekaysoh@yahoo.com>
2005-08-18 13:33:39 +0100

    test fixes for Solaris 2.6
    Solaris 2.6's stock sed doesn't support alternatives in regular
    expression. We workaround by breaking up the regexp.

---
+++ mercurial/fancyopts.py
@@ -0,0 +1,27 @@
+import getopt
+
+def fancyopts(args, options, state):
+    long=[]
+    short=''
+    map={}
+    dt={}
+
+    for s, l, d, c in options:
+        map['-'+s] = map['--'+l]=l
+        state[l] = d
+        dt[l] = type(d)
+        if not d is None and not callable(d): s, l=s+':', l+'='
+        if s: short = short + s
+        if l: long.append(l)
+
+    opts, args = getopt.getopt(args, short, long)
+
+    for opt, arg in opts:
+        if dt[map[opt]] is type(fancyopts): state[map[opt]](state,map[opt],arg)
+        elif dt[map[opt]] is type(1): state[map[opt]] = int(arg)
+        elif dt[map[opt]] is type(''): state[map[opt]] = arg
+        elif dt[map[opt]] is type([]): state[map[opt]].append(arg)
+        elif dt[map[opt]] is type(None): state[map[opt]] = 1
+
+    return args
+
