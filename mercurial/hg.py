d0b1fdb8676b45de9fd30ce5ca9aa63284721432
Josef "Jeff" Sipek <jeffpc@optonline.net>
2006-02-05 20:52:55 -0500

    [hgweb] Added rename information to gitweb templates & misc cleanups Fixed up file log template's title Removed double entry for filelogentry in gitweb map file

---
+++ mercurial/hg.py
@@ -0,0 +1,28 @@
+# hg.py - repository classes for mercurial
+#
+# Copyright 2005 Matt Mackall <mpm@selenic.com>
+#
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+
+from node import *
+from repo import *
+from demandload import *
+demandload(globals(), "localrepo httprepo sshrepo statichttprepo")
+
+def repository(ui, path=None, create=0):
+    if path:
+        if path.startswith("http://"):
+            return httprepo.httprepository(ui, path)
+        if path.startswith("https://"):
+            return httprepo.httpsrepository(ui, path)
+        if path.startswith("hg://"):
+            return httprepo.httprepository(
+                ui, path.replace("hg://", "http://"))
+        if path.startswith("old-http://"):
+            return statichttprepo.statichttprepository(
+                ui, path.replace("old-http://", "http://"))
+        if path.startswith("ssh://"):
+            return sshrepo.sshrepository(ui, path)
+
+    return localrepo.localrepository(ui, path, create)
