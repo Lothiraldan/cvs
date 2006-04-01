cf450bc896b8f9b73aa059d74e532b62f5a9fc6f
Vadim Gelfer <vadim.gelfer@gmail.com>
2006-03-31 17:00:09 -0800

    support nested repositories.
    mercurial will not recurse into a subdirectory that contains a .hg
    directory. it is treated as a separate repository.

---
+++ mercurial/i18n.py
@@ -0,0 +1,15 @@
+"""
+i18n.py - internationalization support for mercurial
+
+Copyright 2005 Matt Mackall <mpm@selenic.com>
+
+This software may be used and distributed according to the terms
+of the GNU General Public License, incorporated herein by reference.
+"""
+
+# the import from gettext is _really_ slow
+# for now we use a dummy function
+gettext = lambda x: x
+#import gettext
+#t = gettext.translation('hg', '/usr/share/locale', fallback=1)
+#gettext = t.gettext
