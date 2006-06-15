6b98ec8ce2803b031dabe643e9c35066a6c46322
Vadim Gelfer <vadim.gelfer@gmail.com>
2006-06-15 16:37:23 -0700

    extend network protocol to stop clients from locking servers
    now all repositories have capabilities slot, tuple with list of names.
    
    if 'unbundle' capability present, repo supports push where client does
    not need to lock server.  repository classes that have unbundle capability
    also have unbundle method.
    
    implemented for ssh now, will be base for push over http.
    
    unbundle protocol acts this way.  server tells client what heads it
    has during normal negotiate step.  client starts unbundle by repeat
    server's heads back to it.  if server has new heads, abort immediately.
    otherwise, transfer changes to server.  once data transferred, server
    locks and checks heads again.  if heads same, changes can be added.
    else someone else added heads, and server aborts.
    
    if client wants to force server to add heads, sends special heads list of
    'force'.

---
+++ contrib/darcs2hg.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# Encoding: iso-8859-1
+# vim: tw=80 ts=4 sw=4 noet
+# -----------------------------------------------------------------------------
+# Project   : Basic Darcs to Mercurial conversion script
+# -----------------------------------------------------------------------------
+# Author    : Sebastien Pierre <sebastien@xprima.com>
+# Creation  : 24-May-2006
+# Last mod  : 26-May-2006
+# History   :
+#             26-May-2006 - Updated
+#             24-May-2006 - First implementation
+# -----------------------------------------------------------------------------
+
+import os, sys
+import tempfile
+import xml.dom.minidom as xml_dom
+from time import strptime, mktime
+
+DARCS_REPO = None
+HG_REPO    = None
+
+USAGE = """\
+%s DARCSREPO HGREPO
+
+    Converts the given Darcs repository to a new Mercurial repository. The given
+    HGREPO must not exist, as it will be created and filled up (this will avoid
+    overwriting valuable data.
+
+""" % (os.path.basename(sys.argv[0]))
+
+# ------------------------------------------------------------------------------
+#
+# Utilities
+#
+# ------------------------------------------------------------------------------
+
+def cmd(text, path=None):
+	"""Executes a command, in the given directory (if any), and returns the
+	command result as a string."""
+	cwd = None
+	if path:
+		path = os.path.abspath(path)
+		cwd  = os.getcwd()
+		os.chdir(path)
+	print text
+	res = os.popen(text).read()
+	if path:
+		os.chdir(cwd)
+	return res
+
+def writefile(path, data):
+	"""Writes the given data into the given file."""
+	f = file(path, "w") ; f.write(data)  ; f.close()
+
+# ------------------------------------------------------------------------------
+#
+# Darcs interface
+#
+# ------------------------------------------------------------------------------
+
+def darcs_changes(darcsRepo):
+	"""Gets the changes list from the given darcs repository. This returns the
+	chronological list of changes as (change name, change summary)."""
+	changes    = cmd("darcs changes --reverse --xml-output", darcsRepo)
+	doc        = xml_dom.parseString(changes)
+	res        = []
+	for patch_node in doc.childNodes[0].childNodes:
+		name = filter(lambda n:n.nodeName == "name", patch_node.childNodes)
+		comm = filter(lambda n:n.nodeName == "comment", patch_node.childNodes)
+		if not name:continue
+		else: name = name[0].childNodes[0].data
+		if not comm: comm = ""
+		else: comm = comm[0].childNodes[0].data
+		author = patch_node.getAttribute("author")
+		date = patch_node.getAttribute("date")
+		yield author, date, name, comm
+
+def darcs_pull(hg_repo, darcs_repo, change):
+	cmd("darcs pull '%s' --all --patches='%s'" % (darcs_repo, change), hg_repo)
+
+# ------------------------------------------------------------------------------
+#
+# Mercurial interface
+#
+# ------------------------------------------------------------------------------
+
+def hg_commit( hg_repo, text, author, date ):
+	fd, tmpfile = tempfile.mkstemp(prefix="darcs2hg_")
+	writefile(tmpfile, text)
+	cmd("hg add -X _darcs", hg_repo)
+	cmd("hg remove -X _darcs --after", hg_repo)
+	cmd("hg commit -l %s -u '%s' -d '%s 0'"  % (tmpfile, author, date), hg_repo)
+	os.unlink(tmpfile)
+
+# ------------------------------------------------------------------------------
+#
+# Main
+#
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+	args = sys.argv[1:]
+	# We parse the arguments
+	if len(args)   == 2:
+		darcs_repo = os.path.abspath(args[0])
+		hg_repo    = os.path.abspath(args[1])
+	else:
+		print USAGE
+		sys.exit(-1)
+	# Initializes the target repo
+	if not os.path.isdir(darcs_repo + "/_darcs"):
+		print "No darcs directory found at: " + darc_repo
+		sys.exit(-1)
+	if not os.path.isdir(hg_repo):
+		os.mkdir(hg_repo)
+	else:
+		print "Given HG repository must not exist. It will be created"
+		sys.exit(-1)
+	cmd("hg init '%s'" % (hg_repo))
+	cmd("darcs initialize", hg_repo)
+	# Get the changes from the Darcs repository
+	for author, date, summary, description in darcs_changes(darcs_repo):
+		text = summary + "\n" + description
+		darcs_pull(hg_repo, darcs_repo, summary)
+		epoch = int(mktime(strptime(date, '%Y%m%d%H%M%S')))
+		hg_commit(hg_repo, text, author, epoch)
+
+# EOF
+
---
+++ mercurial/hgweb/request.py
@@ -0,0 +1,59 @@
+# hgweb/request.py - An http request from either CGI or the standalone server.
+#
+# Copyright 21 May 2005 - (c) 2005 Jake Edge <jake@edge2.net>
+# Copyright 2005 Matt Mackall <mpm@selenic.com>
+#
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+
+from mercurial.demandload import demandload
+demandload(globals(), "socket sys cgi os errno")
+from mercurial.i18n import gettext as _
+
+class hgrequest(object):
+    def __init__(self, inp=None, out=None, env=None):
+        self.inp = inp or sys.stdin
+        self.out = out or sys.stdout
+        self.env = env or os.environ
+        self.form = cgi.parse(self.inp, self.env, keep_blank_values=1)
+        self.will_close = True
+
+    def write(self, *things):
+        for thing in things:
+            if hasattr(thing, "__iter__"):
+                for part in thing:
+                    self.write(part)
+            else:
+                try:
+                    self.out.write(str(thing))
+                except socket.error, inst:
+                    if inst[0] != errno.ECONNRESET:
+                        raise
+
+    def done(self):
+        if self.will_close:
+            self.inp.close()
+            self.out.close()
+        else:
+            self.out.flush()
+
+    def header(self, headers=[('Content-type','text/html')]):
+        for header in headers:
+            self.out.write("%s: %s\r\n" % header)
+        self.out.write("\r\n")
+
+    def httphdr(self, type, filename=None, length=0):
+
+        headers = [('Content-type', type)]
+        if filename:
+            headers.append(('Content-disposition', 'attachment; filename=%s' %
+                            filename))
+        # we do not yet support http 1.1 chunked transfer, so we have
+        # to force connection to close if content-length not known
+        if length:
+            headers.append(('Content-length', str(length)))
+            self.will_close = False
+        else:
+            headers.append(('Connection', 'close'))
+            self.will_close = True
+        self.header(headers)
