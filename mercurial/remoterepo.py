7084ad8ca8af40ba3373fcaacdd4c59848892068
Matt Mackall <mpm@selenic.com>
2006-06-28 15:49:33 -0500

    revlog: handle integer arguments to lookup

---
+++ mercurial/remoterepo.py
@@ -0,0 +1,23 @@
+# remoterepo - remote repositort proxy classes for mercurial
+#
+# Copyright 2005 Matt Mackall <mpm@selenic.com>
+#
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+
+class remoterepository(object):
+    def dev(self):
+        return -1
+
+    def local(self):
+        return False
+
+class remotelock(object):
+    def __init__(self, repo):
+        self.repo = repo
+    def release(self):
+        self.repo.unlock()
+        self.repo = None
+    def __del__(self):
+        if self.repo:
+            self.release()
