1ab793a8f29d833d97c7e6826c4ed22df9abfc40
Vadim Gelfer <vadim.gelfer@gmail.com>
2006-07-23 09:04:14 -0700

    addremove: do not deprecate. command is too useful to drop. is also best place to try autodetect of rename/copy by content similarity.

---
+++ mercurial/node.py
@@ -0,0 +1,22 @@
+"""
+node.py - basic nodeid manipulation for mercurial
+
+Copyright 2005 Matt Mackall <mpm@selenic.com>
+
+This software may be used and distributed according to the terms
+of the GNU General Public License, incorporated herein by reference.
+"""
+
+from demandload import demandload
+demandload(globals(), "binascii")
+
+nullid = "\0" * 20
+
+def hex(node):
+    return binascii.hexlify(node)
+
+def bin(node):
+    return binascii.unhexlify(node)
+
+def short(node):
+    return hex(node[:6])
