11f250a819c823a367cebe3f875fda40b23cae96
Thomas Arendsen Hein <thomas@intevation.de>
2008-10-19 19:49:56 +0200

    Fix Debian bug #494889 (fetching from static-http://... broken)
    Changeset 6605a03cbf87 which fixed issue965 (hg clone static-http
    does not work for empty repos) broke cloning of repos with old layout
    (without store) via static-http.
    
    This fix makes cloning from old-style repositories possible again,
    but will not allow cloning of empty old-style repositories as this
    can not be detected reliably.

---
+++ tests/test-doctest.py
@@ -0,0 +1,16 @@
+# this is hack to make sure no escape characters are inserted into the output
+import os;
+if 'TERM' in os.environ:
+    del os.environ['TERM'] 
+import doctest
+
+import mercurial.changelog
+# test doctest from changelog
+
+doctest.testmod(mercurial.changelog)
+
+import mercurial.httprepo
+doctest.testmod(mercurial.httprepo)
+
+import mercurial.util
+doctest.testmod(mercurial.util)
