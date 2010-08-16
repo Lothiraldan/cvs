57d764e862ceaa3cd8748be1bdd9d232bc39eda6
Nicolas Dumazet <nicdumz.commits@gmail.com>
2010-08-16 10:53:52 +0900

    tests: unify test-mq-strip

---
+++ tests/test-doctest.py
@@ -0,0 +1,22 @@
+# this is hack to make sure no escape characters are inserted into the output
+import os
+if 'TERM' in os.environ:
+    del os.environ['TERM']
+import doctest
+
+import mercurial.changelog
+# test doctest from changelog
+
+doctest.testmod(mercurial.changelog)
+
+import mercurial.httprepo
+doctest.testmod(mercurial.httprepo)
+
+import mercurial.util
+doctest.testmod(mercurial.util)
+
+import mercurial.dagparser
+doctest.testmod(mercurial.dagparser, optionflags=doctest.NORMALIZE_WHITESPACE)
+
+import hgext.convert.cvsps
+doctest.testmod(hgext.convert.cvsps)
