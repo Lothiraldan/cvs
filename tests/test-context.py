d2c3d3f74381f5778e93a20d75e05b3105ea52dd
Mads Kiilerich <madski@unity3d.com>
2014-12-31 14:45:02 +0100

    tests: add test coverage for lfdirstate invalidation of linear update
    f72d73937853 introduced a significant performance regression: All largefiles
    are marked 'normallookup' in lfdirstate by linear (or noop) updates and has to
    be rehashed by the next command.
    
    To avoid such regressions, keep an eye on the dirstate content after a plain
    'hg up'.

---
+++ tests/test-context.py
@@ -0,0 +1,52 @@
+import os
+from mercurial import hg, ui, context, encoding
+
+u = ui.ui()
+
+repo = hg.repository(u, 'test1', create=1)
+os.chdir('test1')
+
+# create 'foo' with fixed time stamp
+f = open('foo', 'wb')
+f.write('foo\n')
+f.close()
+os.utime('foo', (1000, 1000))
+
+# add+commit 'foo'
+repo[None].add(['foo'])
+repo.commit(text='commit1', date="0 0")
+
+print "workingfilectx.date =", repo[None]['foo'].date()
+
+# test memctx with non-ASCII commit message
+
+def filectxfn(repo, memctx, path):
+    return context.memfilectx(repo, "foo", "")
+
+ctx = context.memctx(repo, ['tip', None],
+                     encoding.tolocal("Gr\xc3\xbcezi!"),
+                     ["foo"], filectxfn)
+ctx.commit()
+for enc in "ASCII", "Latin-1", "UTF-8":
+    encoding.encoding = enc
+    print "%-8s: %s" % (enc, repo["tip"].description())
+
+# test performing a status
+
+def getfilectx(repo, memctx, f):
+    fctx = memctx.parents()[0][f]
+    data, flags = fctx.data(), fctx.flags()
+    if f == 'foo':
+        data += 'bar\n'
+    return context.memfilectx(repo, f, data, 'l' in flags, 'x' in flags)
+
+ctxa = repo.changectx(0)
+ctxb = context.memctx(repo, [ctxa.node(), None], "test diff", ["foo"],
+                      getfilectx, ctxa.user(), ctxa.date())
+
+print ctxb.status(ctxa)
+
+# test performing a diff on a memctx
+
+for d in ctxb.diff(ctxa, git=True):
+    print d
