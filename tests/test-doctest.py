c866a285816a4b3746135f4cc391bad859b748f5
FUJIWARA Katsunori <foozy@lares.dti.ne.jp>
2015-07-30 06:16:12 +0900

    localrepo: make journal.dirstate contain in-memory changes before transaction
    Before this patch, in-memory dirstate changes aren't written out at
    opening transaction, even though 'journal.dirstate' is created
    directly from '.hg/dirstate'.
    
    Therefore, subsequent 'hg rollback' uses incomplete 'undo.dirstate' to
    restore dirstate, if dirstate is changed and isn't written out before
    opening transaction.
    
    In cases below, the condition "dirstate is changed and isn't written
    out before opening transaction" isn't satisfied and this problem
    doesn't appear:
    
      - "wlock scope" and "transaction scope" are almost equivalent
    
        e.g. 'commit --amend', 'import' and so on
    
      - dirstate changes are written out before opening transaction
    
        e.g. 'rebase' (via 'dirstateguard') and 'commit -A' (by separated
        wlock scopes)
    
    On the other hand, 'backout' may satisfy the condition above.
    
    To make 'journal.dirstate' contain in-memory changes before opening
    transaction, this patch explicitly invokes 'dirstate.write()' in
    'localrepository.transaction()'.
    
    'dirstate.write()' is placed before not "writing journal files out"
    but "invoking pretxnopen hooks" for visibility of dirstate changes to
    external hook processes.
    
    BTW, in the test script, 'touch -t 200001010000' and 'hg status' are
    invoked to make file 'c' surely clean in dirstate, because "clean but
    unsure" files indirectly cause 'dirstate.write()' at 'repo.status()'
    in 'repo.commit()' (see fe03f522dda9 for detail) and prevents from
    certainly reproducing the issue.

---
+++ tests/test-doctest.py
@@ -0,0 +1,39 @@
+# this is hack to make sure no escape characters are inserted into the output
+import os, sys
+if 'TERM' in os.environ:
+    del os.environ['TERM']
+import doctest
+
+def testmod(name, optionflags=0, testtarget=None):
+    __import__(name)
+    mod = sys.modules[name]
+    if testtarget is not None:
+        mod = getattr(mod, testtarget)
+    doctest.testmod(mod, optionflags=optionflags)
+
+testmod('mercurial.changelog')
+testmod('mercurial.dagparser', optionflags=doctest.NORMALIZE_WHITESPACE)
+testmod('mercurial.dispatch')
+testmod('mercurial.encoding')
+testmod('mercurial.hg')
+testmod('mercurial.hgweb.hgwebdir_mod')
+testmod('mercurial.match')
+testmod('mercurial.minirst')
+testmod('mercurial.patch')
+testmod('mercurial.pathutil')
+testmod('mercurial.parser')
+testmod('mercurial.revset')
+testmod('mercurial.store')
+testmod('mercurial.subrepo')
+testmod('mercurial.templatefilters')
+testmod('mercurial.templater')
+testmod('mercurial.ui')
+testmod('mercurial.url')
+testmod('mercurial.util')
+testmod('mercurial.util', testtarget='platform')
+testmod('hgext.convert.convcmd')
+testmod('hgext.convert.cvsps')
+testmod('hgext.convert.filemap')
+testmod('hgext.convert.p4')
+testmod('hgext.convert.subversion')
+testmod('hgext.mq')
---
+++ tests/test-wireproto.py
@@ -0,0 +1,48 @@
+from mercurial import wireproto
+
+class proto(object):
+    def __init__(self, args):
+        self.args = args
+    def getargs(self, spec):
+        args = self.args
+        args.setdefault('*', {})
+        names = spec.split()
+        return [args[n] for n in names]
+
+class clientpeer(wireproto.wirepeer):
+    def __init__(self, serverrepo):
+        self.serverrepo = serverrepo
+    def _call(self, cmd, **args):
+        return wireproto.dispatch(self.serverrepo, proto(args), cmd)
+
+    @wireproto.batchable
+    def greet(self, name):
+        f = wireproto.future()
+        yield {'name': mangle(name)}, f
+        yield unmangle(f.value)
+
+class serverrepo(object):
+    def greet(self, name):
+        return "Hello, " + name
+
+    def filtered(self, name):
+        return self
+
+def mangle(s):
+    return ''.join(chr(ord(c) + 1) for c in s)
+def unmangle(s):
+    return ''.join(chr(ord(c) - 1) for c in s)
+
+def greet(repo, proto, name):
+    return mangle(repo.greet(unmangle(name)))
+
+wireproto.commands['greet'] = (greet, 'name',)
+
+srv = serverrepo()
+clt = clientpeer(srv)
+
+print clt.greet("Foobar")
+b = clt.batch()
+fs = [b.greet(s) for s in ["Fo, =;:<o", "Bar"]]
+b.submit()
+print [f.value for f in fs]
